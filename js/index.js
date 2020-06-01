4d47986977a49780b7d9d6422c3925fb7fe273bd
Shylesh S <37534898+shyleshsunnithan@users.noreply.github.com>
2020-06-01 15:30:05 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const navLinks = document.querySelectorAll('.nav__link')
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
-});
+}); 
 
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
